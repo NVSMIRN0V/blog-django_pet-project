44c493c8a5ec77c044214dd802975923375bd6bd
Nikita Smirnov <job.nvsmirnov@gmail.com>
2024-04-29 20:44:42 +0300

    routing for users configed

--- users/views.py
+++ users/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+
+
+def login(request):
+    return render(request, 'users/login.html', context={'title': 'Login'})
+
+
+def register(request):
+    return render(request, 'users/register.html', context={'title': 'Register'})
